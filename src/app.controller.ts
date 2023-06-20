4babb7315d55185f32983176ad3e9064e2b8dc53
Wesley Gado <wesleygado@MacBook-Air-de-Wesley.local>
2023-06-20 16:14:51 -0300

    4 - Controller e requisições

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('alunos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getHello() {
+    const aluno = {
+      id: '1',
+      nome: 'Wesley',
+    };
+    return aluno;
   }
 }
